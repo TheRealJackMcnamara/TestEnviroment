e7d06b1cd12ac08acb9c0a70c3ceb5b3fbc07a8f
TheRealJackMcnamara <jjmcnama@my.loyno.edu>
2025-06-12 17:40:23 -0500

    Add files via upload

---
+++ HW04.cpp
@@ -0,0 +1,12 @@
+// Program Name: Small World
+// Purpose: Extned programming functionality and experience
+// Author: Jack McNamara
+// Date Last Modified: June 12, 2025
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "******************************" <<endl;
+    cout << "*Jack McNamara *" << endl;
+    cout << "******************************" <<endl;
+    return 0;
+}
